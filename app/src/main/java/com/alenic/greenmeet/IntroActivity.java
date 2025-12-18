0b938ccd042742332c189227f896797b524db35e
AL3X-AFK <alexvvaldezv@gmail.com>
2025-12-18 11:53:58 +0100

    Start intro activity

--- app/src/main/java/com/alenic/greenmeet/IntroActivity.java
+++ app/src/main/java/com/alenic/greenmeet/IntroActivity.java
@@ -7,18 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager2.widget.ViewPager2;
 
-public class OnboardingActivity extends AppCompatActivity {
-
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding);
-
-
+        setContentView(R.layout.activity_intro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
-
