a32a47c61d51f931dd728653ebc08357dafb41dc
Nicoooo26 <nicogpfutbol@gmail.com>
2026-01-15 11:30:21 +0100

    fragments en branch principio

--- app/src/main/java/com/alenic/greenmeet/Login.java
+++ app/src/main/java/com/alenic/greenmeet/Login.java
@@ -1,12 +1,16 @@
 package com.alenic.greenmeet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 public class Login extends AppCompatActivity {
 
@@ -15,10 +19,17 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        MaterialButton btnLogin = findViewById(R.id.btnLogin);
+        TextView txtPregunta = findViewById(R.id.txtPregunta);
+
+        txtPregunta.setOnClickListener( v -> {
+            Intent intent = new Intent(Login.this, Register.class);
+            startActivity(intent);
+        });
+        btnLogin.setOnClickListener( v -> {
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/alenic/greenmeet/Register.java
+++ app/src/main/java/com/alenic/greenmeet/Register.java
@@ -1,12 +1,16 @@
 package com.alenic.greenmeet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 public class Register extends AppCompatActivity {
 
@@ -15,10 +19,18 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView txtPregunta = findViewById(R.id.txtPregunta);
+        MaterialButton btnRegister = findViewById(R.id.btnRegister);
+
+        txtPregunta.setOnClickListener( v -> {
+            Intent intent = new Intent(Register.this, Login.class);
+            startActivity(intent);
+        });
+
+        btnRegister.setOnClickListener( v -> {
+            Intent intent = new Intent(Register.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
