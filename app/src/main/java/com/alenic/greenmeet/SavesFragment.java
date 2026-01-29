c1be70afd97b6e08ed3eba232930ab6c554298b7
AL3X-AFK <alexvvaldezv@gmail.com>
2026-01-29 13:59:18 +0100

    some changes

---
+++ app/src/main/java/com/alenic/greenmeet/SavesFragment.java
@@ -0,0 +1,18 @@
+package com.alenic.greenmeet;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SavesFragment extends Fragment {
+
+
+    public SavesFragment() {
+        // Required empty public constructor
+    }
+
+}
