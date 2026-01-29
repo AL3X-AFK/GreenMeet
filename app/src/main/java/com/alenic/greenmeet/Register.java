eaf6d36bb3f43ec07c410fa00611d1c6a736b7a0
Nicoooo26 <nicogpfutbol@gmail.com>
2026-01-29 13:57:34 +0100

    intent connect Firebase

--- app/src/main/java/com/alenic/greenmeet/Register.java
+++ app/src/main/java/com/alenic/greenmeet/Register.java
@@ -2,6 +2,9 @@ package com.alenic.greenmeet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,8 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText signinEmail,signinPassword;
+    private Button signinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +28,23 @@ public class Register extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
+        auth = FirebaseAuth.getInstance();
+        signinButton = findViewById(R.id.btnRegister);
+        signinEmail = findViewById(R.id.etEmail);
+        signinPassword = findViewById(R.id.etNombre);
+
         TextView txtPregunta = findViewById(R.id.txtPregunta);
-        MaterialButton btnRegister = findViewById(R.id.btnRegister);
 
         txtPregunta.setOnClickListener( v -> {
             Intent intent = new Intent(Register.this, Login.class);
             startActivity(intent);
         });
 
-        btnRegister.setOnClickListener( v -> {
-            Intent intent = new Intent(Register.this, MainActivity.class);
-            startActivity(intent);
-        });
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        })
     }
 }
