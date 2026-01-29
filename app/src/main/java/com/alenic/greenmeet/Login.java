d6f5a28c505465296fd839cb136f031842b26fa8
Nicoooo26 <nicogpfutbol@gmail.com>
2026-01-29 20:46:41 +0100

    connect with Firebase and Authenticator

--- app/src/main/java/com/alenic/greenmeet/Login.java
+++ app/src/main/java/com/alenic/greenmeet/Login.java
@@ -2,17 +2,33 @@ package com.alenic.greenmeet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText loginEmail,loginPassword;
+    private TextView txtRedirect;
+    private MaterialButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +36,48 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-        MaterialButton btnLogin = findViewById(R.id.btnLogin);
-        TextView txtPregunta = findViewById(R.id.txtPregunta);
+        auth = FirebaseAuth.getInstance();
+        btnLogin = findViewById(R.id.btnLogin);
+        txtRedirect = findViewById(R.id.txtPregunta);
+        loginEmail = findViewById(R.id.etEmail);
+        loginPassword = findViewById(R.id.etPassword);
 
-        txtPregunta.setOnClickListener( v -> {
+        txtRedirect.setOnClickListener( v -> {
             Intent intent = new Intent(Login.this, Register.class);
             startActivity(intent);
         });
-        btnLogin.setOnClickListener( v -> {
-            Intent intent = new Intent(Login.this, MainActivity.class);
-            startActivity(intent);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if(!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(email,pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(Login.this,"Inicio de sesión exitoso",Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(Login.this,MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this,"Inicio de sesión fallida",Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else{
+                        loginPassword.setError("Este campo no puede estar vacío.");
+                    }
+                } else if(email.isEmpty()){
+                    loginEmail.setError("Este campo no puede estar vacío");
+                } else {
+                    loginEmail.setError("Introduce un email válido");
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/alenic/greenmeet/Register.java
+++ app/src/main/java/com/alenic/greenmeet/Register.java
@@ -6,21 +6,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private EditText signinEmail,signinPassword;
-    private Button signinButton;
+    private MaterialButton signinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,7 @@ public class Register extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         signinButton = findViewById(R.id.btnRegister);
         signinEmail = findViewById(R.id.etEmail);
-        signinPassword = findViewById(R.id.etNombre);
+        signinPassword = findViewById(R.id.etPassword);
 
         TextView txtPregunta = findViewById(R.id.txtPregunta);
 
@@ -43,8 +48,28 @@ public class Register extends AppCompatActivity {
         signinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String user = signinEmail.getText().toString().trim();
+                String pass = signinPassword.getText().toString().trim();
 
+                if(user.isEmpty()){
+                    signinEmail.setError("Este campo es obligatorio");
+                }
+                if(pass.isEmpty()){
+                    signinPassword.setError("Este campo es obligatorio");
+                } else{
+                    auth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(Register.this,"Registro exitoso",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Register.this,Login.class));
+                            }else{
+                                Toast.makeText(Register.this,"Registro fallido "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
-        })
+        });
     }
 }
